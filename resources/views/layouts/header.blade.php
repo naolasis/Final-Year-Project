48c4fbb8f85503d593f193ca5ff108e82c6dee46
Eyuel <hypertimelink@gmail.com>
2024-03-26 14:42:22 +0300

    add layout php files

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,6 @@
+
+<header>
+    <h2 style="font-weight:400; font-family: system-ui, -apple-system">Lorem ipsum dolor sit amet.</h2>
+    <h2 style="font-weight:400; font-family: Roboto;">Lorem ipsum dolor sit amet.</h2>
+    <h2 style="font-weight:400; font-family: Outfit">Lorem ipsum dolor sit amet.</h2>
+</header>
